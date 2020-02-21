ab2eb42b639c7d6fef1c5e2f5c00465a8f72958e
MosammatHabibaRahman <60427978+MosammatHabibaRahman@users.noreply.github.com>
2020-02-21 19:49:04 +0600

    Add files via upload

--- Form.php
+++ Form.php
@@ -7,6 +7,11 @@
 		<title>Registration Page</title>
 	</head>
 	<body>
+		</br>
+		</br>
+		</br>
+		</br>
+		</br>
 		<form method="POST" action="Check.php">
 			<center>
 				<table border=1 width=350>
@@ -15,12 +20,12 @@
 					</tr>
 					<tr>
 						<td>Name</td>
-						<td width=200><input type="text" name="name" value="" size=10/></td>
+						<td width=200><input type="text" name="name" value=""/></td>
 						<td width=25></td>
 					</tr>
 					<tr>
 						<td>Email</td>
-						<td width=200><input type="text" name="email" value="" size=10/></td>
+						<td width=200><input type="text" name="email" value=""/></td>
 						<td width=25></td>
 					</tr>
 					<tr>
@@ -35,17 +40,18 @@
 					<tr>
 						<td>Date of Birth</td>
 						<td width=200>
-							<input type="text" name="day" value="" size=1/>/
-							<input type="text" name="month" value="" size=1/>/
-							<input type="text" name="year" value="" size=1/>
-							<i>dd/mm/yyyy</i>
+							<input type="text" name="day" value="" size=3/>/
+							<input type="text" name="month" value="" size=3/>/
+							<input type="text" name="year" value="" size=3/>
+							<i> dd/mm/yyyy</i>
 						</td>
 						<td width=25></td>
 					</tr>
 					<tr>
 						<td>Blood Group</td>
 						<td width=200>
-							<select>
+							<select name="select">
+								<option></option>
 								<option>A+</option>
 								<option>B+</option>
 								<option>AB</option>
@@ -57,17 +63,17 @@
 					<tr>
 						<td>Degree</td>
 						<td width=200>
-							<input type="checkbox" name="SSC">SSC/>
-							<input type="checkbox" name="HSC">HSC/>
-							<input type="checkbox" name="BSc">BSc./>
-							<input type="checkbox" name="MSc">MSc./>
+							<input type="checkbox" name="ssc"/>SSC
+							<input type="checkbox" name="hsc"/>HSC
+							<input type="checkbox" name="bsc"/>BSc.
+							<input type="checkbox" name="msc"/>MSc.
 						</td>
 						<td width=25></td>
 					</tr>
 					<tr>
 						<td>Photo</td>
 						<td colspan=2 width=150>
-							<input type="file" name="Browse" value="Browse"/>
+							<input type="file" id="browse" name="browse"/>
 						</td>
 					</tr>
 					<tr>
